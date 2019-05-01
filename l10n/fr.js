69fd08300a70cdee2b57e138313e7ffad3ba9279
PaulLereverend <nte.remix@gmail.com>
2019-05-01 17:58:28 +0200

    add french

---
+++ l10n/fr.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "extract",
+    {
+    "Extract here" : "Extraire ici",
+    "Error extracting " : "Erreur lors de l'extraction",
+    "Extract" : "Extraire",
+    "Extract archive from the web interface" : "Extraire des archives depuis l'interface web"
+},
+"");
