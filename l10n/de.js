7111128b01f6a0ae73b70c78728ac2810b3bd8b0
JulianStange <49147127+JulianStange@users.noreply.github.com>
2019-05-01 15:47:29 +0200

    Create de.js

---
+++ l10n/de.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "extract",
+    {
+    "Extract here" : "Hier Entpacken",
+    "Error extracting " : "Fehlen beim entpacken",
+    "Extract" : "Entpacke",
+    "Extract archive from the web interface" : "Entpacke Archive aus dem Web Interface"
+},
+"");
