9d3ff3693d06ec9029387640071c7a20f0025a18
Paul <mail@paulrosenberg.de>
2019-06-26 19:17:43 +0200

    Update de_DE.js

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -2,8 +2,18 @@ OC.L10N.register(
     "extract",
     {
     "Extract here" : "Hier Entpacken",
-    "Error extracting " : "Fehler beim Entpacken",
+    "Error extracting " : "Fehler beim Entpacken ",
     "Extract" : "Entpacke",
-    "Extract archive from the web interface" : "Entpacke Archiv aus dem Web Interface"
+    "Extract archive from the web interface" : "Entpacke Archiv aus dem Web Interface",
+    "Zip extension is not available" : "Zip Erweiterung ist nicht verfügbar",
+    "Can't find zip file" : "Zip-Datei kann nicht gefunden werden",
+    "Can't open zip file at %s" : "Zip-Datei kann unter %s nicht geöffnet werden",
+    "rar extension or unrar is not installed or available" : "Rar oder Unrar Erweiterung ist nicht installiert oder verfügbar",
+    "Can't find rar file" : "Rar-Datei kann nicht gefunden werden",
+    "Can't find rar file at %s" : "Rar-Datei kann unter %s nicht geöffnet werden",
+    "p7zip and p7zip-full are not installed or available" : "p7zip und p7zip-full Erweiterung ist nicht installiert oder verfügbar",
+    "Can't find archive on external local storage" : "Das Archiv kann nicht auf einem externen lokalen Speicher gefunden werden.",
+    "Can't find archive at %s" : "Archiv-Datei kann unter %s nicht gefunden werden",
+    "Can't scan file at %s" : "Die Datei kann nicht in %s gescannt werden."
 },
 "");
