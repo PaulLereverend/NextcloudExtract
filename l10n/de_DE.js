931590a6802ee0ae8f13cd8359770e91707752d5
Paul <mail@paulrosenberg.de>
2019-06-26 18:22:08 +0200

    Create de_DE.js

---
+++ l10n/de_DE.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "extract",
+    {
+    "Extract here" : "Hier Entpacken",
+    "Error extracting " : "Fehler beim Entpacken",
+    "Extract" : "Entpacke",
+    "Extract archive from the web interface" : "Entpacke Archiv aus dem Web Interface"
+},
+"");
