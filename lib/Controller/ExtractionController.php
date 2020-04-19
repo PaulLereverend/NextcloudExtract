e39494b2ff02d3bf82aa4b188810d9bfad4d9d10
Paul <paulereverend@gmail.com>
2020-04-19 14:44:25 +0200

    Add debug infos

--- lib/Controller/ExtractionController.php
+++ lib/Controller/ExtractionController.php
@@ -116,7 +116,8 @@ class ExtractionController extends Controller {
 					}else{
 							exec('unrar x ' .escapeshellarg($externalMP.$directory. '/' .$nameOfFile). ' -R ' .escapeshellarg($externalMP.$directory). ' -o+',$output,$return);
 							if (sizeof($output) == 0){
-								$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("rar extension or unrar is not installed or available")));
+								$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("rar extension or unrar is not installed or available \n
+								DEBUG(".$return.")".$output)));
 								return json_encode($response);
 							}else{
 								$response = array_merge($response, array("code" => 1));
@@ -153,7 +154,8 @@ class ExtractionController extends Controller {
 				exec('unrar x ' .escapeshellarg($file). ' -R ' .escapeshellarg($dir). ' -o+',$output,$return);
 				if(sizeof($output) <= 4){
 					if (file_exists($file)){
-						$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("rar extension or unrar is not installed or available")));
+						$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("rar extension or unrar is not installed or available\n
+						DEBUG(".$return.")".$output)));
 						return json_encode($response);
 					}else{
 						$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("Can't find rar file at ").$file));
@@ -198,7 +200,8 @@ class ExtractionController extends Controller {
 						exec('7za -y x ' .escapeshellarg($externalMP.$directory. '/' .$nameOfFile). ' -o' .escapeshellarg($externalMP.$directory. '/' .pathinfo($nameOfFile)['filename']. '/'), $output,$return);
 					}
 					if(sizeof($output) <= 5){
-						$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("p7zip and p7zip-full are not installed or available")));
+						$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("p7zip and p7zip-full are not installed or available\n
+						DEBUG(".$return.")".$output)));
 						return json_encode($response);
 					}else{
 						$response = array_merge($response, array("code" => 1));
@@ -227,7 +230,8 @@ class ExtractionController extends Controller {
 			}
 			if(sizeof($output) <= 5){
 				if (file_exists($file)){
-					$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("p7zip and p7zip-full are not installed or available")));
+					$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("p7zip and p7zip-full are not installed or available\n
+					DEBUG(".$return.")".$output)));
 					return json_encode($response);
 				}else{
 					$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("Can't find archive at ").$file));
