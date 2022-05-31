6e0fd3385cbf53995cd06d7a4288ee8680512a8b
agnisa <arne.gnisa@capgemini.com>
2022-05-31 13:11:52 +0200

    Add duplicate error dialog on extraction

--- js/extraction.js
+++ js/extraction.js
@@ -1,84 +1,114 @@
 $(document).ready(function () {
 
     var actionsExtract = {
+        createDialog: function (title, init, callback) {
+            return OC.dialogs.confirmHtml(
+                '',
+                title,
+                callback,
+                true
+            ).then(() => {
+                var $dialog = $('.oc-dialog:visible');
+                var $content = $('.oc-dialog-content');
+                var $buttons = $dialog.find('button');
+
+                var $cancelButton = $buttons.eq(0);
+                var $confirmButton = $buttons.eq(1);
+
+                $content.empty();
+
+                $cancelButton.text(t('core', 'Cancel'));
+                $confirmButton.text(t('core', 'Confirm'));
+
+                init($dialog, $content, $cancelButton, $confirmButton);
+            });
+        },
+
+        extractDialog: function (filename, context, type) {
+            var self = this;
+
+            var dirName = filename;
+            var matches = dirName.match('^([^\\.]+)');
+            if (matches) {
+                dirName = matches[0];
+            }
+
+            var data = {
+                sourceFileName: filename,
+                targetDirName: dirName,
+                directory: context.dir,
+                external: context.fileInfoModel.attributes.mountType && context.fileInfoModel.attributes.mountType.startsWith("external") ? 1 : 0,
+                type: type
+            };
+
+            var tr = context.fileList.findFileEl(filename);
+            context.fileList.showFileBusyState(tr, true);
+
+            var $input = $('<input/>');
+            self.createDialog(
+                t('extract', 'Extract'),
+                ($dialog, $content, $cancelButton, $confirmButton) => {
+                    $confirmButton.text(t('extract', 'Extract'));
+
+                    var $text = $('<p/>');
+                    $text.text(t('extract', 'Files will be extracted to this folder:'));
+                    $content.append($text);
+
+                    $input.attr('type', 'text').attr('id', 'file-name-input').attr('placeholder', t('extract', 'File Name')).attr('value', dirName);
+                    $content.append($input);
+                },
+                (result) => {
+                    context.fileList.showFileBusyState(tr, false);
+                    data.targetDirName = $input.val();
+                    if (result) {
+                        $.ajax({
+                            type: "POST",
+                            async: "false",
+                            url: OC.filePath('extract', 'ajax', 'extract.php'),
+                            data: data,
+                            success: function (response) {
+                                console.log(response);
+                                if (response.code === 1) {
+                                    context.fileList.reload();
+                                } else {
+                                    context.fileList.showFileBusyState(tr, false);
+                                    OC.dialogs.alert(
+                                        t('extract', response.desc),
+                                        t('extract', 'Error extracting' ) + " " + filename
+                                    );
+                                }
+                            }
+                        });
+                    }
+                },
+            );
+        },
+
         init: function () {
             var self = this;
+
             OCA.Files.fileActions.registerAction({
                 name: 'extractzip',
-                displayName: t('extract', 'Extract here'),
+                displayName: t('extract', 'Extract'),
                 mime: 'application/zip',
                 permissions: OC.PERMISSION_UPDATE,
                 type: OCA.Files.FileActions.TYPE_DROPDOWN,
                 iconClass: 'icon-extract',
                 actionHandler: function (filename, context) {
-                    var data = {
-                        nameOfFile: filename,
-                        directory: context.dir,
-                        external: context.fileInfoModel.attributes.mountType && context.fileInfoModel.attributes.mountType.startsWith("external") ? 1 : 0,
-                        type: 'zip'
-                    };
-                    var tr = context.fileList.findFileEl(filename);
-                    context.fileList.showFileBusyState(tr, true);
-                    $.ajax({
-                        type: "POST",
-                        async: "false",
-                        url: OC.filePath('extract', 'ajax', 'extract.php'),
-                        data: data,
-                        success: function (element) {
-                            console.log(element);
-                            element = element.replace(/null/g, '');
-                            response = JSON.parse(element);
-                            if (response.code == 1) {
-                                context.fileList.reload();
-                            } else {
-                                context.fileList.showFileBusyState(tr, false);
-                                OC.dialogs.alert(
-                                    t('extract', response.desc),
-                                    t('extract', 'Error extracting ' + filename)
-                                );
-                            }
-                        }
-                    });
+                    self.extractDialog(filename, context, 'zip');
                 }
             });
 
             // RAR
             OCA.Files.fileActions.registerAction({
                 name: 'extractrar',
-                displayName: t('extract', 'Extract here'),
+                displayName: t('extract', 'Extract'),
                 mime: 'application/x-rar-compressed',
                 permissions: OC.PERMISSION_UPDATE,
                 type: OCA.Files.FileActions.TYPE_DROPDOWN,
                 iconClass: 'icon-extract',
                 actionHandler: function (filename, context) {
-                    var data = {
-                        nameOfFile: filename,
-                        directory: context.dir,
-                        external: context.fileInfoModel.attributes.mountType && context.fileInfoModel.attributes.mountType.startsWith("external") ? 1 : 0,
-                        type: 'rar'
-                    };
-                    var tr = context.fileList.findFileEl(filename);
-                    context.fileList.showFileBusyState(tr, true);
-                    $.ajax({
-                        type: "POST",
-                        async: "false",
-                        url: OC.filePath('extract', 'ajax', 'extract.php'),
-                        data: data,
-                        success: function (element) {
-                            element = element.replace(/null/g, '');
-                            console.log(element);
-                            response = JSON.parse(element);
-                            if (response.code == 1) {
-                                context.fileList.reload();
-                            } else {
-                                context.fileList.showFileBusyState(tr, false);
-                                OC.dialogs.alert(
-                                    t('extract', response.desc),
-                                    t('extract', 'Error extracting ' + filename)
-                                );
-                            }
-                        }
-                    });
+                    self.extractDialog(filename, context, 'rar');
                 }
             });
             // TAR
@@ -88,39 +118,13 @@ $(document).ready(function () {
             types.forEach(type => {
                 OCA.Files.fileActions.registerAction({
                     name: 'extractOthers',
-                    displayName: t('extract', 'Extract here'),
+                    displayName: t('extract', 'Extract'),
                     mime: type,
                     permissions: OC.PERMISSION_UPDATE,
                     type: OCA.Files.FileActions.TYPE_DROPDOWN,
                     iconClass: 'icon-extract',
                     actionHandler: function (filename, context) {
-                        var data = {
-                            nameOfFile: filename,
-                            directory: context.dir,
-                            external: context.fileInfoModel.attributes.mountType && context.fileInfoModel.attributes.mountType.startsWith("external") ? 1 : 0,
-                            type: 'other'
-                        };
-                        var tr = context.fileList.findFileEl(filename);
-                        context.fileList.showFileBusyState(tr, true);
-                        $.ajax({
-                            type: "POST",
-                            async: "false",
-                            url: OC.filePath('extract', 'ajax', 'extract.php'),
-                            data: data,
-                            success: function (element) {
-                                element = element.replace('null', '');
-                                response = JSON.parse(element);
-                                if (response.code == 1) {
-                                    context.fileList.reload();
-                                } else {
-                                    context.fileList.showFileBusyState(tr, false);
-                                    OC.dialogs.alert(
-                                        t('extract', response.desc),
-                                        t('extract', 'Error extracting ' + filename)
-                                    );
-                                }
-                            }
-                        });
+                        self.extractDialog(filename, context, 'other');
                     }
                 });
             });
