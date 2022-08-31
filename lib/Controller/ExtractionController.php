206b89854edff17634a385077938048713d9d2fc
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-08-31 13:02:03 +0200

    Modularize and cleanup
    - move the actual extractors to a separate file in order to have the
      controller code smaller
    - use the DataResponse class form NC instead of own JSON encode/decode
    - cleanup JS and merge the 3-fold duplicated code into one loop
    - Controller: use the IRootFolder. Why was tearDownFS() needed?
    
    TODO: find a cleaner way to trigger the rescan of the extracted folder.

--- lib/Controller/ExtractionController.php
+++ lib/Controller/ExtractionController.php
@@ -2,10 +2,10 @@
 namespace OCA\Extract\Controller;
 
 use OCP\IRequest;
-use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Controller;
 use OCP\Files\NotFoundException;
+use OCP\Files\IRootFolder;
 use ZipArchive;
 use Rar;
 use PharData;
@@ -14,31 +14,66 @@ use OCP\IL10N;
 use OCP\EventDispatcher\IEventDispatcher;
 use OC\Files\Filesystem;
 use OCP\Encryption\IManager;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use OCA\Extract\Service\ExtractionService;
 
 class ExtractionController extends Controller {
 
 	/** @var IL10N */
 	private $l;
 
+	/** @var LoggerInterface */
+	private $logger;
+
+	/** @var IRootFolder */
+	private $rootFolder;
+
 	/** @var IManager */
 	protected $encryptionManager;
 
+	/** @var string */
 	private $userId;
 
-	public function __construct($AppName, IRequest $request, IL10N $l, IManager $encryptionManager, $UserId){
+	/**  @var ExtractionService */
+	private $extractionService;
+
+	public function __construct(
+		string $AppName
+		, IRequest $request
+		, ExtractionService $extractionService
+		, IRootFolder $rootFolder
+		, IL10N $l
+		, LoggerInterface $logger
+		, IManager $encryptionManager
+		, $UserId
+	){
 		parent::__construct($AppName, $request);
 		$this->l = $l;
+		$this->logger = $logger;
 		$this->encryptionManager = $encryptionManager;
 		$this->userId = $UserId;
+		$this->extractionService = $extractionService;
+		$this->rootFolder = $rootFolder;
 	}
 
-
-	public function getFile($directory, $fileName){
-		\OC_Util::tearDownFS();
-		\OC_Util::setupFS($this->userId);
-		return Filesystem::getView()->getLocalFile($directory . '/' . $fileName);
+	private function getFile($directory, $fileName){
+		$userFolder = $this->rootFolder->getUserFolder($this->userId);
+		$fileNode = $userFolder->get($directory . '/' . $fileName);
+		return $fileNode->getStorage()->getLocalFile($fileNode->getInternalPath());
 	}
 
+	//Register the new files to the NC filesystem
+	private static function postExtract($filename, $directory, $tmpPath, $external){
+		$NCDestination = $directory . '/' . $filename;
+		if($external){
+			Filesystem::mkdir($tmpPath);
+			Filesystem::rename($tmpPath, $NCDestination);
+			Filesystem::rmdir(dirname($tmpPath));
+		}else{
+			Filesystem::mkdir($NCDestination);
+		}
+	}
 
 	/**
 	 * CAUTION: the @Stuff turns off security checks; for this page no admin is
@@ -58,17 +93,17 @@ class ExtractionController extends Controller {
 		if ($this->encryptionManager->isEnabled()) {
 			$response = array();
 			$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("Encryption is not supported yet")));
-			return json_encode($response);
+			return new DataResponse($response);
 		}
 		$file = $this->getFile($directory, $nameOfFile);
 		$dir = dirname($file);
-		//name of the file without extention
-		$filename = pathinfo($nameOfFile)['filename'];
+		//name of the file without extension
+		$filename = pathinfo($nameOfFile, PATHINFO_FILENAME);
 		$extractTo = $dir . '/' . $filename;
 		$tmpPath = "/extract_tmp/" . $filename ;
 
-		if(array_key_exists('extension', pathinfo($filename)) && pathinfo($filename)['extension'] == "tar"){
-			$tmpPath = '/extract_tmp/' . pathinfo($filename)['filename'];
+		if(pathinfo($filename, PATHINFO_EXTENSION) == "tar"){
+			$tmpPath = '/extract_tmp/' . pathinfo($filename, PATHINFO_FILENAME);
 		}
 
 		// if the file is un external storage
@@ -78,98 +113,31 @@ class ExtractionController extends Controller {
 
 		switch ($type) {
 			case 'zip':
-				$response = $this->extractZip($file, $filename, $extractTo);
+				$response = $this->extractionService->extractZip($file, $filename, $extractTo);
 				break;
 			case 'rar':
-				$response = $this->extractRar($file, $filename, $extractTo);
+				$response = $this->extractionService->extractRar($file, $filename, $extractTo);
 				break;
 			default:
 				// Check if the file is .tar.gz in order to do the extraction on a single step
-				if(array_key_exists('extension', pathinfo($filename)) && pathinfo($filename)['extension'] == "tar"){
-					$clean_filename = pathinfo($filename)['filename'];
+				if(pathinfo($filename, PATHINFO_EXTENSION) == "tar"){
+					$clean_filename = pathinfo($filename, PATHINFO_FILENAME);
 					$extractTo = dirname($extractTo) . '/' . $clean_filename;
-					$response = $this->extractOther($file, $clean_filename, $extractTo);
-					$file = $extractTo . '/' . pathinfo($file)['filename'];
+					$response = $this->extractionService->extractOther($file, $clean_filename, $extractTo);
+					$file = $extractTo . '/' . pathinfo($file, PATHINFO_FILENAME);
 					$filename = $clean_filename;
-					$response = $this->extractOther($file, $filename, $extractTo);
-					
+					$response = $this->extractionService->extractOther($file, $filename, $extractTo);
+
 					// remove .tar file
 					unlink($file);
 				}else{
-					$response = $this->extractOther($file, $filename, $extractTo);
+					$response = $this->extractionService->extractOther($file, $filename, $extractTo);
 				}
 				break;
 		}
 
-		$this->postExtract($filename, $directory, $tmpPath, $external);
-		return $response;
-	}
-	public function extractZip($file, $filename, $extractTo){
-		$response = array();
+		self::postExtract($filename, $directory, $tmpPath, $external);
 
-		if (!extension_loaded("zip")){
-			$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("Zip extension is not available")));
-			return json_encode($response);
-		}
-
-		$zip = new ZipArchive();
-
-		if (!$zip->open($file) === TRUE){
-			$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("Cannot open Zip file")));
-			return json_encode($response);
-		}
-
-		$zip->extractTo($extractTo);
-		$zip->close();
-		$response = array_merge($response, array("code" => 1));
-		return json_encode($response);
-	}
-	public function extractRar($file, $filename, $extractTo){
-		$response = array();
-
-		if (!extension_loaded("rar")){
-			exec('unrar x ' .escapeshellarg($file). ' -R ' .escapeshellarg($extractTo). '/ -o+',$output,$return);
-				if(sizeof($output) <= 4){
-					$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("Oops something went wrong. Check that you have rar extension or unrar installed")));
-					return json_encode($response);
-				}
-		}else{
-			$rar_file = rar_open($file);
-			$list = rar_list($rar_file);
-			foreach($list as $archive_file) {
-				$entry = rar_entry_get($rar_file, $archive_file->getName());
-				$entry->extract($extractTo);
-			}
-			rar_close($rar_file);
-		}
-
-		$response = array_merge($response, array("code" => 1));
-		return json_encode($response);
-	}
-	public function extractOther($file, $filename, $extractTo){
-		$response = array();
-
-		exec('7za -y x ' .escapeshellarg($file). ' -o' .escapeshellarg($extractTo),$output,$return);
-
-		if(sizeof($output) <= 5){
-			$response = array_merge($response, array("code" => 0, "desc" => $this->l->t("Oops something went wrong. Check that you have p7zip installed")));
-			error_log($output);
-
-			return json_encode($response);
-		}
-		$response = array_merge($response, array("code" => 1));
-		return json_encode($response);
-	}
-
-	//Register the new files to the NC filesystem
-	public function postExtract($filename, $directory, $tmpPath, $external){
-		$NCDestination = $directory . '/' . $filename;
-		if($external){
-			Filesystem::mkdir($tmpPath);
-			Filesystem::rename($tmpPath, $NCDestination);
-			Filesystem::rmdir(dirname($tmpPath));
-		}else{
-			Filesystem::mkdir($NCDestination);
-		}
+		return new DataResponse($response);
 	}
 }
